894e70a17174212a4de31b2202bfbd0fec34ff17
jpmurray32 <jpmurray32@gmail.com>
2018-09-15 10:38:06 -0500

    Bug Fixing and Timer

---
+++ app.js
@@ -0,0 +1,335 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+var server = app.listen(port, function() {
+});
+
+app.use(express.static('client'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "client/index.html");
+});
+
+var io = socket(server);
+
+var players = {};
+var names = {};
+var flags = {};
+var redscore = 0;
+var bluescore = 0;
+var minutes = 15;
+var seconds = 0;
+
+function flag(team, x, y) {
+    this.team = team;
+    this.x = x;
+    this.y = y;
+    this.id = Math.random();
+    this.taken = false;
+    var checked = false;
+    while (!checked) {
+        checked = true;
+        this.id = Math.random();
+        for (var i in flags) {
+            if (this.id == flags[i].id) {
+                checked = false;
+            }
+        }
+    }
+    flags[this.id] = this;
+    this.flagCheck = function() {
+        if (this.y >= 990 && this.team == "red" || this.y <= 1000 && this.team == "blue") {
+            if (this.team == "blue") {
+                redscore++;
+            } else {
+                bluescore++;
+            }
+            io.emit("scores", {red: redscore, blue: bluescore});
+            this.newPos();
+        }
+    }
+    this.newPos = function() {
+        if (this.team == "blue") {
+            this.x = Math.floor(Math.random() * 1990);
+            this.y = Math.floor(Math.random() * 490) + 1500;
+        } else {
+            this.y = Math.floor(Math.random() * 490);
+            this.x = Math.floor(Math.random() * 1990);
+        }
+    }
+}
+
+var redone = new flag("red", 0, 0);
+redone.newPos();
+var redtwo = new flag("red", 0, 0);
+redtwo.newPos();
+var redthree = new flag("red", 0, 0);
+redthree.newPos();
+
+var blueone = new flag("blue", 0, 0);
+blueone.newPos();
+var bluetwo = new flag("blue", 0, 0);
+bluetwo.newPos();
+var bluethree = new flag("blue", 0, 0);
+bluethree.newPos();
+
+io.on('connection', function(socket) {
+    var nameList = [];
+    for (var i in names) {
+        nameList.push(names[i]);
+    }
+    socket.emit('names', nameList);
+
+    socket.on('start', function(data) {
+        socket.emit('scores', {red: redscore, blue: bluescore});
+
+        var checked = false;
+        while (!checked) {
+            checked = true;
+            socket.id = Math.random();
+            for (var i in players) {
+                if (players[i].id == socket.id) {
+                    checked = false;
+                }
+            }
+        }
+        players[socket.id] = socket;
+        names[socket.id] = data.name;
+        socket.dead = false;
+        socket.holding = null;
+
+        socket.idle = 0;
+        socket.count = 0;
+    
+        var red = 0;
+        var blue = 0;
+        for (var i in players) {
+            var p = players[i];
+            if (p.team == "red") {
+                red++;
+            } else if (p.team == "blue") {
+                blue++;
+            }
+        }
+        if (red > blue) {
+            socket.team = "blue";
+        } else {
+            socket.team = "red";
+        }
+
+        socket.name = data.name;
+
+        socket.x = Math.floor(Math.random() * 1900);
+        if (socket.team == "red") {
+            socket.y = Math.floor(Math.random() * 948 + 1); 
+        } else {
+            socket.y = Math.floor(Math.random() * 948 + 1001);
+        }
+
+        socket.info = [];
+
+        socket.upPress = false;
+        socket.downPress = false;
+        socket.leftPress = false;
+        socket.rightPress = false;
+        socket.speed = 8;
+        socket.vel = {
+            up: 0,
+            down: 0,
+            left: 0,
+            right: 0,
+        }
+
+        socket.positionCheck = function() {
+            if (socket.upPress) {
+                socket.vel.up = socket.speed;
+            } 
+            if (socket.downPress) {
+                socket.vel.down = socket.speed;
+            } 
+            if (socket.leftPress) {
+                socket.vel.left = socket.speed;
+            } 
+            if (socket.rightPress) {
+                socket.vel.right = socket.speed;
+            }
+
+            if (socket.vel.up > socket.vel.down) {
+                socket.vel.up = socket.vel.up - socket.vel.down;
+                socket.vel.down = 0;
+            } else if (socket.vel.up < socket.vel.down) {
+                socket.vel.down = socket.vel.down - socket.vel.up;
+                socket.vel.up = 0;
+            }
+            if (socket.vel.left > socket.vel.right) {
+                socket.vel.left = socket.vel.left - socket.vel.right;
+                socket.vel.right = 0;
+            } else if (socket.vel.left < socket.vel.right) {
+                socket.vel.right = socket.vel.right - socket.vel.left;
+                socket.vel.left = 0;
+            }
+        }
+
+        socket.positionChange = function() {
+            socket.y += socket.vel.down - socket.vel.up;
+            socket.x += socket.vel.right - socket.vel.left;
+            socket.vel.up *= 0.93;
+            socket.vel.down *= 0.93;
+            socket.vel.left *= 0.93;
+            socket.vel.right *= 0.93;
+        }
+
+        socket.collision = function() {
+            for (var i in players) {
+                var p = players[i];
+                var s = socket;
+
+                if (p.id != s.id) {
+                    var changex = (s.x + 15) - (p.x + 15);
+                    var changey = (s.y + 15) - (p.y + 15);
+                    if (changex**2 + changey**2 <= 60**2 && !p.dead) {
+                        if (socket.y > 950 && socket.team == "red" && p.team != s.team || socket.y < 1000 && socket.team == "blue" && p.team != s.team) {
+                            socket.dead = true;
+                            if (socket.holding != null) {
+                                socket.holding.newPos();
+                            }
+                            socket.emit("collided");
+                        }
+                    }
+                }
+
+                if (p.x <= 25) {
+                    p.vel.left = 0;
+                }
+                if (p.x >= 1950) {
+                    p.vel.right = 0;
+                }
+                if (p.y <= 25) {
+                    p.vel.up = 0;
+                }
+                if (p.y >= 1950) {
+                    p.vel.down = 0;
+                }
+            }
+
+            var holds = false;
+            for (var i in flags) {
+                var s = socket;
+                var f = flags[i];
+
+                if (f.team != s.team) {
+                    var changex = (s.x + 15) - (f.x + 5);
+                    var changey = (s.y + 15) - (f.y + 5);
+                    if (changex**2 + changey**2 <= 40**2 && !p.dead) {
+                        if (p.holding == f || p.holding == null) {
+                            holds = true;
+                            s.holding = f;
+                            f.x = s.x + 10;
+                            f.y = s.y + 10;
+                        }
+                    }
+                }
+            }
+            if (!holds) {
+                s.holding = null;
+            }
+        }
+
+        socket.emit('player', {
+            name: socket.name,
+            x: socket.x,
+            y: socket.y,
+            team: socket.team,
+        });
+
+        socket.on('disconnect', function() {
+            delete players[socket.id];
+            delete names[socket.id];
+        });
+
+        socket.on('keyPress', function(data) {
+            socket.idle = 0;
+            if (data.key == "up") {
+                socket.upPress = data.down;
+            } else if (data.key == "down") {
+                socket.downPress = data.down;
+            } else if (data.key == "left") {
+                socket.leftPress = data.down;
+            } else if (data.key == "right") {
+                socket.rightPress = data.down;
+            }
+        });
+    });
+});
+
+setInterval(function() {
+    var flagInfo = [];
+    for (var i in flags) {
+        flags[i].flagCheck();
+        flagInfo.push({
+            team: flags[i].team,
+            x: flags[i].x,
+            y: flags[i].y,
+        })
+    }
+    for (var i in players) {
+        players[i].emit('flags', flagInfo);
+    }
+    for (var i in players) {
+        var p = players[i];
+        p.info = [];
+        p.positionCheck();
+        p.collision();
+        p.positionChange();
+        p.info.push({
+            x: p.x,
+            y: p.y,
+            name: p.name,
+            team: p.team,
+            dead: p.dead,
+        });
+        for (var o in players) {
+            if (players[o] != p) {
+                p.info.push({
+                    x: players[o].x,
+                    y: players[o].y,
+                    name: players[o].name,
+                    team: players[o].team,
+                    dead: players[o].dead,
+                });
+            }
+        }
+        p.emit('players', p.info);
+        p.count++;
+        if (p.count == 24) {
+            p.count = 0;
+            p.idle++;
+            if (p.idle >= 60) {
+                p.emit('idle');
+            }
+        }
+    }
+}, 1000/24);
+
+setInterval(function() {
+    if (seconds == 0) {
+        minutes--;
+        if (minutes != 0) {
+            seconds = 60;
+        }
+    }
+    seconds--;
+    io.emit("timer", {
+        minutes: minutes,
+        seconds: seconds,
+    });
+
+    if (minutes == 0 && seconds == 0) {
+        minutes = 15;
+        redscore = 0;
+        bluescore = 0;
+        io.emit("scores", {red: redscore, blue: bluescore});
+    }
+}, 1000);
---
+++ client/js/index.js
@@ -0,0 +1,267 @@
+var socket = io();
+var player;
+var redScore = 0;
+var blueScore = 0;
+var minutes = 0;
+var seconds = 0;
+var flags = [];
+
+var names;
+socket.on('names', function(data) {
+    names = data;
+});
+
+function object(name, team, x, y, w, h) {
+    this.name = name;
+    this.team = team;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.dead = false;
+}
+
+var draw = function(ctx, color, name, x, y, w, h) {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.arc(x + w/2, y + h/2, 30, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = "white";
+    ctx.lineWidth = 3;
+    ctx.stroke();
+    ctx.font = "bolder 30px Verdana";
+    var width = ctx.measureText(name).width;
+    ctx.fillStyle = "white";
+    ctx.fillText(name, x - ((width - 30)/2), y - 20);
+    ctx.closePath();
+}
+
+var drawFlag = function(ctx, color, x, y, w, h) {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.arc(x + w/2, y + h/2, w, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
+function posx(x) {
+    return ((window.innerWidth / 2 - 15) + (x - player.x));
+}
+
+function posy(y) {
+    return ((window.innerHeight / 2 - 15) + (y - player.y));
+}
+
+socket.on('players', function(data) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var mini = document.getElementById('mini');
+    var mctx = mini.getContext('2d');
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    mctx.clearRect(0, 0, 80, 80);
+    var scoreboard = document.getElementById('scoreboard');
+    var sctx = scoreboard.getContext('2d');
+    sctx.beginPath();
+    sctx.fillStyle = "red";
+    sctx.fillRect(0, 0, 200, 100);
+    sctx.fillStyle = "white";
+    sctx.font = "bolder 60px Verdana";
+    sctx.fillText(redScore.toString(), 80, 70);
+    sctx.fillStyle = "blue";
+    sctx.fillRect(300, 0, 200, 100);
+    sctx.fillStyle = "white";
+    sctx.font = "bolder 60px Verdana";
+    sctx.fillText(blueScore.toString(), 380, 70);
+    sctx.fillStyle = "purple";
+    sctx.fillRect(200, 25, 100, 50);
+    sctx.fillStyle = "white";
+    sctx.font = "bolder 30px Verdana";
+    if (minutes >= 10) {
+        if (seconds < 10) {
+            sctx.fillText(minutes + ":" + "0" + seconds, 200, 60);
+        } else {
+            sctx.fillText(minutes + ":" + seconds, 200, 60);
+        }
+    } else {
+        if (seconds < 10) {
+            sctx.fillText("0" + minutes + ":" + "0" + seconds, 200, 60);
+        } else {
+            sctx.fillText("0" + minutes + ":" + seconds, 200, 60);
+        }
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].name == player.name) {
+            player.x = data[i].x;
+            player.y = data[i].y;
+            ctx.beginPath();
+            ctx.fillStyle = 'lightgray';
+            ctx.fillRect(posx(0), posy(0), 2000, 2000);
+            ctx.fillStyle = "black";
+            ctx.rect(posx(0), posy(0), 2000, 2000);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.beginPath();
+            ctx.fillStyle = "red";
+            ctx.globalAlpha = 0.5;
+            ctx.fillRect(posx(0), posy(0), 2000, 1000);
+            ctx.globalAlpha = 1;
+            ctx.rect(posx(0), posy(0), 2000, 1000);
+            ctx.stroke();
+            ctx.fillStyle = "blue";
+            ctx.globalAlpha = 0.5;
+            ctx.fillRect(posx(0), posy(1000), 2000, 1000);
+            ctx.globalAlpha = 1;
+            ctx.rect(posx(0), posy(1000), 2000, 1000);
+            ctx.stroke();
+            ctx.fillStyle = "black";
+            ctx.fillRect(posx(0), posy(998), 2000, 4);
+            ctx.closePath();
+            mctx.beginPath();
+            mctx.fillStyle = player.team;
+            mctx.arc(player.x / 25, player.y / 25, 3, 0, 2 * Math.PI);
+            mctx.fill();
+            mctx.closePath();
+        } else {
+            if (!data[i].dead) {
+                draw(ctx, data[i].team, data[i].name, posx(data[i].x), posy(data[i].y), 30, 30);
+            }
+        }
+    }
+    if (!player.dead) {
+        draw(ctx, player.team, player.name, window.innerWidth / 2 - 15, window.innerHeight / 2 - 15, 30, 30);
+    }
+    for (var i = 0; i < flags.length; i++) {
+        drawFlag(ctx, flags[i].team, posx(flags[i].x), posy(flags[i].y), 10, 10);
+    }
+});
+
+socket.on('player', function(data) {
+    player = new object(data.name, data.team, data.x, data.y, 30, 30);
+});
+
+socket.on('collided', function() {
+    if (!player.dead) {
+        player.dead = true;
+        dead();
+    }
+});
+
+socket.on('scores', function(data) {
+    redScore = data.red;
+    blueScore = data.blue;
+});
+
+socket.on('flags', function(data) {
+    flags = data;
+});
+
+socket.on('idle', function() {
+    location.reload();
+});
+
+socket.on('timer', function(data) {
+    minutes = data.minutes;
+    seconds = data.seconds;
+    if (minutes == 0 && seconds == 0) {
+        if (redScore > blueScore) {
+            document.getElementById("final").innerHTML = "Red Wins<br>" + redScore + " to " + blueScore;
+        } else if (blueScore > redScore) {
+            document.getElementById("final").innerHTML = "Blue Wins<br>" + blueScore + " to " + redScore;
+        } else {
+            document.getElementById("final").innerHTML = "It's a tie!";
+        }
+    
+        setTimeout(function() {
+            document.getElementById('final').innerHTML = "";
+        }, 3000);
+    }
+});
+
+function dead() {
+    var opacity = 0;
+    document.getElementById('cover').style.display = "block";
+    var interval = setInterval(function() {
+        opacity+=0.05;
+        document.getElementById('cover').style.opacity = opacity;
+        if (opacity >= 1) {
+            clearInterval(interval);
+            location.reload();
+        }
+    }, 1000/24);
+}
+
+function play(username) {
+    socket.emit('start', {
+        name: username,
+    });
+}
+
+window.onkeydown = function(e) {
+    idle = 0;
+    if (e.keyCode == 38 || e.keyCode == 87) {
+        socket.emit('keyPress', {key: "up", down: true});
+    } else if (e.keyCode == 40 || e.keyCode == 83) {
+        socket.emit('keyPress', {key: "down", down: true});
+    } else if (e.keyCode == 37 || e.keyCode == 65) {
+        socket.emit('keyPress', {key: "left", down: true});
+    } else if (e.keyCode == 39 || e.keyCode == 68) {
+        socket.emit('keyPress', {key: "right", down: true});
+    }
+
+    var input = document.getElementById('name').value;
+    if (e.keyCode == 13 && input.length > 0) {
+        var safe = true;
+        for (var i = 0; i < input.length; i++) {
+            if (input.charAt(i) == "<") {
+                safe = false;
+            }
+        }
+        for (var i = 0; i < names.length; i++) {
+            if (input == names[i]) {
+                safe = false;
+                alert("Sorry, that name is in use.");
+                input = "";
+            }
+        }
+        if (safe) {
+            play(input);
+            input = "";
+            document.getElementById('play').style.display = "block";
+            document.getElementById('login').style.display = "none";
+        }
+    }
+}
+
+window.onkeyup = function(e) {
+    if (e.keyCode == 38 || e.keyCode == 87) {
+        socket.emit('keyPress', {key: "up", down: false});
+    } else if (e.keyCode == 40 || e.keyCode == 83) {
+        socket.emit('keyPress', {key: "down", down: false});
+    } else if (e.keyCode == 37 || e.keyCode == 65) {
+        socket.emit('keyPress', {key: "left", down: false});
+    } else if (e.keyCode == 39 || e.keyCode == 68) {
+        socket.emit('keyPress', {key: "right", down: false});
+    }
+}
+
+window.onunload = function() {
+    socket.emit('disconnect');
+}
+
+window.onresize = function() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth - 1;
+    canvas.height = window.innerHeight - 1;
+}
+
+window.onload = function() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth - 1;
+    canvas.height = window.innerHeight - 1;
+}
+
+setInterval(function() {
+    if (idle >= 100) {
+        location.reload();
+    }
+}, 1000);
